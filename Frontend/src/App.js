4716a4fa0bfed6f0fc8b1891a66dd57e0bc6c4ff
Linuka Lolitha Jayesinghe <it21274398@my.sliit.lk>
2025-02-07 18:29:18 +0530

    fixed file strucher

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,15 +4,15 @@ import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 //Admin imports
-import AdminSignup from "./pages/AdminSignup";
-import AdminLogin from "./pages/AdminLogin";
-import AddProduct from "./pages/AddProduct";
-import ProductList from "./pages/ProductList";
-import AdminDashboard from "./pages/AdminDashboard";
+import AdminSignup from "./pages/Admin/AdminSignup";
+import AdminLogin from "./pages/Admin/AdminLogin";
+import AddProduct from "./pages/Admin/AddProduct";
+import ProductList from "./pages/Admin/ProductList";
+import AdminDashboard from "./pages/Admin/AdminDashboard";
 
 //Customer imports
-import CustProduct from "./pages/CustProductList";
-import CustomerOrder from "./pages/CustomerOrder";
+import CustProduct from "./pages/Customer/CustProductList";
+import CustomerOrder from "./pages/Customer/CustomerOrder";
 
 //Other imports
 import NavBar from "./components/Navbar";
@@ -23,17 +23,22 @@ const App = () => {
   return (
     <Router>
       <NavBar />
-      <ToastContainer position="top-right" autoClose={2000} />
+      <ToastContainer position="top-right" autoClose={1500} />
       <Routes>
-        <Route path="/admin/signup" element={<AdminSignup />} />
+        {/* Admin Routes */}
         <Route path="/" element={<AdminLogin />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/admin/signup" element={<AdminSignup />} />
         <Route path="/admin/ProductAdd" element={<AddProduct />} />
         <Route path="/admin/ProductList" element={<ProductList />} />
-        <Route path="/CustProductList" element={<CustProduct />} />
         <Route path="/admin/AdminDashboard" element={<AdminDashboard />} />
+
+        {/* Customer Routes */}
+        <Route path="/CustProductList" element={<CustProduct />} />
         <Route path="/CustomerOrder" element={<CustomerOrder />} />
+
+        {/* Other Routes */}
+        <Route path="/Home" element={<Home />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
       </Routes>
     </Router>
   );
